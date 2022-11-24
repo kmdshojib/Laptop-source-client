0fb3156d04b7b6469bbda5dedc46ecadf3a2cb5f
kmdshojib <kmd.shojib@gmail.com>
2022-11-24 21:38:11 +0600

    log out added

--- src/context/authcontext.js
+++ src/context/authcontext.js
@@ -1,12 +1,14 @@
-import React, { createContext } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import {app} from '../firebase/firebase'
 
-import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword} from 'firebase/auth'
+import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile} from 'firebase/auth'
 
 export const AuthContext = createContext()
 const auth = getAuth(app)
 
 const UserContext = ({children}) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
     
     // Register the user
 
@@ -18,8 +20,24 @@ const UserContext = ({children}) => {
     const signIn = (email,password)=>{
         return signInWithEmailAndPassword(auth,email,password)
     }
+    // log out an user
+    const logOut = ()=>{
+        signOut(auth)
+    }
+    // updating current user
 
-    const authInfo ={signIn,signUp}
+    const updateUserProfile = (profile) => updateProfile(auth.currentUser, profile)
+
+    useEffect(()=>{
+        const  unSubscribe = onAuthStateChanged(auth, (currentUser)=>{
+            setUser(currentUser)
+            setLoading(false)
+            console.log("currentUser",currentUser)
+        })
+        return () => unSubscribe
+    })
+
+    const authInfo ={signIn,signUp,logOut,updateUserProfile,user,loading}
     return ( 
         <AuthContext.Provider value={authInfo}>
             {children}
