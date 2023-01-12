e5357ae1863ba1a60244c441d7453ecabe4bc614
kmdshojib <kmd.shojib@gmail.com>
2023-01-12 23:06:01 +0600

    banner img removed

--- src/components/Home/homebanner.js
+++ src/components/Home/homebanner.js
@@ -4,7 +4,7 @@ const Banner = () => {
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <img alt="" src="https://www.digitaltrends.com/wp-content/uploads/2021/11/macbook-pro-2021-16.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+
                 <div>
                     <h1 className="text-5xl font-bold mb-2 font-mono">
                         <Typewriter
