1d6d8c6daf8115a9a4c6c5fbf0c81a53944678bd
kmdshojib <kmd.shojib@gmail.com>
2023-06-15 00:48:42 +0600

    banner fixed

--- src/components/Home/homebanner.js
+++ src/components/Home/homebanner.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Typewriter from 'typewriter-effect';
 
 import "./homebanner.css"
+
 const Banner = () => {
     return (
         <div className="hero min-h-screen homebg">
             <div className="hero-content flex-col lg:flex-row-reverse">
 
-                <div>
-                    <h1 className="text-4xl font-bold mb-2 font-mono">
+                <div className='w-[600px] md:w-[700px]'>
+                    <h1 className="text-2xl md:text-4xl font-bold mb-2 font-mono">
                         <Typewriter
                             options={{
                                 strings: ['Sell Your Laptop right here!', 'Buye your best Laptop from here!'],
@@ -17,12 +18,10 @@ const Banner = () => {
                             }}
                         />
                     </h1>
-                    <p className="py-3 text-lg font-medium">One of the best place for buying and selling your laptop.</p>
-                    <p className="py-3 text-lg font-medium">Get the best price for your laptop.</p>
-                    <p className="py-3 text-lg font-medium">We offer you best after sales service</p>
+                    <p className="text-sm md:text-xl py-3 font-medium">One of the best place for buying and selling your laptop.</p>
+                    <p className="py-3 text-sm md:text-xl font-medium">Get the best price for your laptop.</p>
+                    <p className="py-3 text-sm md:text-xl font-medium">We offer you best after sales service</p>
                     <button className="btn btn-primary mt-2">Let's find a laptop for you!</button>
-
-
                 </div>
             </div>
         </div>
